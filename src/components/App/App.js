d6b165ff76301834cdd1bb00a415c974f613c3f6
Gada Wako <gada.wakooo@gmail.com>
2020-10-03 17:36:37 -0500

    test commit

---
+++ src/components/App/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {HashRouter as Router, Route} from 'react-router-dom';
+import './App.css';
+import Home from '../Home/Home';
+import Edit from '../Edit/Edit';
+import Details from '../Details/Details';
+
+class App extends Component {
+  // Renders the entire app on the DOM
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Movies!</h1>
+        <Router>
+          {/* ADD PAGES! */}
+        </Router>
+        <p>Empty Page</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Details/Details.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Details extends Component {
+  // Renders the entire app on the DOM
+
+  render() {
+    return (
+      <div>
+          <h1>Details</h1>
+      </div>
+    );
+  }
+}
+
+export default Details;
